68b574707854b19acf68b1fbb750761064c3a1d0
Irfan <irfanalamt@gmail.com>
2021-06-24 12:12:47 -0400

    material UI alignment

--- src/signup/signup.js
+++ src/signup/signup.js
@@ -1,9 +1,56 @@
 import React, { Component } from "react";
+import {
+  FormControl,
+  InputLabel,
+  Input,
+  Paper,
+  CssBaseline,
+  Typography,
+  Button,
+  Container,
+  Box,
+} from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles";
 
 class SignupComponent extends Component {
   render() {
-    return <div>Hello from Signup </div>;
+    return (
+      <main>
+        <Container maxWidth="sm">
+          <CssBaseline></CssBaseline>
+          <Paper
+            style={{
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
+            <Box m={4}></Box>
+            <Typography component="h1" variant="h5">
+              Signup
+            </Typography>
+            <form onSubmit={(e) => this.submitSignup(e)}>
+              <FormControl required margin="normal">
+                <InputLabel htmlFor="signup-email-input" m={4}>
+                  Enter Your Email
+                </InputLabel>
+                <Input
+                  autoComplete="email"
+                  onChange={(e) => this.userTyping("email", e)}
+                  autoFocus
+                ></Input>
+              </FormControl>
+            </form>
+          </Paper>
+        </Container>
+      </main>
+    );
   }
+
+  submitSignup = (e) => {
+    console.log("Submitting signup");
+  };
 }
 
 export default SignupComponent;
